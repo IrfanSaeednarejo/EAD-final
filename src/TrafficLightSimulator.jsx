9e1a4af0294c33d85a15f85ef322499a89b54126
IrfanSaeednarejo <irfansaeed.narejo@gmail.com>
2025-06-03 11:50:29 +0500

    Style: enhance UI with Transitions and active styles

--- src/TrafficLightSimulator.jsx
+++ src/TrafficLightSimulator.jsx
@@ -1,47 +1,51 @@
-
 import { useEffect, useState } from 'react'
 import './App.css'
-
 
 function TrafficLightSimulator() {
   const [color, setColor] = useState("green");
 
-
-
   useEffect(() => {
     const interval = setInterval(() => {
       setColor((prevColor) => {
-
         if (prevColor === "red") return "yellow";
         if (prevColor === "green") return "red";
         return "green";
       });
-    }, 3000); 
+    }, 3000);
 
-    return () => clearInterval(interval); // Cleanup on unmount
-  }
-  , [color]);
-  
+    return () => clearInterval(interval);
+  }, [color]);
+
   return (
     <>
+      <div className="flex flex-col items-center justify-center w-screen h-screen bg-gray-800 text-white">
+        <h1 className="text-4xl font-bold text-center mb-8">Traffic Light Simulator</h1>
 
+        <div className="flex flex-col items-center justify-center w-1/4 space-y-4 bg-gray-700 p-8 rounded-lg shadow-lg">
+          <div className={`w-16 h-16 rounded-full transition-all duration-500 
+          ${
+            color === "red" 
+              ? "bg-red-500 shadow-[0_0_25px_8px_rgba(239,68,68,0.7)]" 
+              : "bg-gray-500"}
+              `}></div>
 
-          
+          <div className={`w-16 h-16 rounded-full transition-all duration-500 
+          ${
+            color === "yellow" 
+              ? "bg-yellow-400 shadow-[0_0_25px_8px_rgba(250,204,21,0.7)]" 
+              : "bg-gray-500 "}
+              `}></div>
 
-          <div className=" items-center justify-center w-screen h-screen bg-gray-800 text-white">
-            <h1 className="text-4xl font-bold text-center">Traffic Light Simulator</h1>
-           <div className="flex  flex-col items-center w-1/4 justify-center space-y-4 bg-gray-700 p-8 rounded-lg shadow-lg mt-8">
-            <div className={`bg-${color === "red" ? "red" : "gray"}-500 w-16 h-16 rounded-full transition-colors duration-500`}></div>
-            <div className={`bg-${color === "yellow" ? "yellow" : "gray"}-500 w-16 h-16 rounded-full transition-colors duration-500`}></div>
-            <div className={`bg-${color === "green" ? "green" : "gray"}-500 w-16 h-16 rounded-full transition-colors duration-500`}></div>
-
-           </div>
-            
-           </div>
-
+          <div className={`w-16 h-16 rounded-full transition-all duration-500
+           ${
+            color === "green" 
+              ? "bg-green-500 shadow-[0_0_25px_8px_rgba(34,197,94,0.7)]" 
+              : "bg-gray-500" }
+          `}></div>
+        </div>
+      </div>
     </>
-    
-  )
+  );
 }
 
-export default TrafficLightSimulator
+export default TrafficLightSimulator;
